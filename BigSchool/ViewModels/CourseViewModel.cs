32c8fd73679b7ffce1f73122444f50e96b7218f9
Mauquoc1904 <mauquocnguyen@gmail.com>
2023-03-15 18:25:55 +0700

    Hoan thanh trang 35

--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using BigSchool.Models;
+using Microsoft.Build.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +9,20 @@ namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Category { get; set; }
+        [Required]
         public IEnumerable<Category> Categories { get; set; }
 
         public DateTime GetDateTime()
